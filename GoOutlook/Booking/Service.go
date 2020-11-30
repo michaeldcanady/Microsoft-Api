eca9ad956fe14392b932960393c8e0c71b974bb1
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2020-11-30 09:56:00 -0500

    Update Service.go

--- GoOutlook/Booking/Service.go
+++ GoOutlook/Booking/Service.go
@@ -4,6 +4,20 @@ import(
 
 )
 
-type Service struct{
-  
+type Service struct {
+  "defaultDuration": "String (timestamp)",
+  "defaultLocation": {"@odata.type": "microsoft.graph.location"},
+  "defaultPrice": 1024,
+  "defaultPriceType": "string",
+  "defaultReminders": [{"@odata.type": "microsoft.graph.bookingReminder"}],
+  "description": "String",
+  "displayName": "String",
+  "emailAddress": "String",
+  "id": "String (identifier)",
+  "isHiddenFromCustomers": true,
+  "notes": "String",
+  "postBuffer": "String (timestamp)",
+  "preBuffer": "String (timestamp)",
+  "schedulingPolicy": {"@odata.type": "microsoft.graph.bookingSchedulingPolicy"},
+  "staffMemberIds": ["String"]
 }
