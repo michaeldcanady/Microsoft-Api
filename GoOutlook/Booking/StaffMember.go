5d438736051fba3a9ce7088c1cdc38d0604da1a0
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2020-11-30 10:05:34 -0500

    Update StaffMember.go

--- GoOutlook/Booking/StaffMember.go
+++ GoOutlook/Booking/StaffMember.go
@@ -5,7 +5,14 @@ import(
 )
 
 type StaffMember struct{
-
+  "availabilityIsAffectedByPersonalCalendar": true,
+  "colorIndex": 1024,
+  "displayName": "String",
+  "emailAddress": "String",
+  "id": "String (identifier)",
+  "role": "string",
+  "useBusinessHours": true,
+  "workingHours": [{"@odata.type": "microsoft.graph.bookingWorkHours"}]
 }
 
 func (s *StaffMember) LIST() []StaffMember{
