17beca00b062a06d9e5ffd522598414aaa8d74ae
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2020-11-30 08:49:01 -0500

    Update Business.go

--- GoOutlook/Booking/Business.go
+++ GoOutlook/Booking/Business.go
@@ -3,3 +3,18 @@ package booking
 import(
 
 )
+
+type Business struct{
+  "address": {"@odata.type": "microsoft.graph.physicalAddress"},
+  "businessHours": [{"@odata.type": "microsoft.graph.bookingWorkHours"}],
+  "businessType": "String",
+  "defaultCurrencyIso": "String",
+  "displayName": "String",
+  "email": "String",
+  "id": "String (identifier)",
+  "isPublished": true,
+  "phone": "String",
+  "publicUrl": "String",
+  "schedulingPolicy": {"@odata.type": "microsoft.graph.bookingSchedulingPolicy"},
+  "webSiteUrl": "String"
+}
