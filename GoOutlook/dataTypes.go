5c58c1da00de560ab30ba919246fc357642fa054
Michael Canady <64753032+michaeldcanady@users.noreply.github.com>
2020-11-30 08:56:29 -0500

    Update dataTypes.go

--- GoOutlook/dataTypes.go
+++ GoOutlook/dataTypes.go
@@ -3,3 +3,11 @@ package Outlook
 import(
 
 )
+
+type bookingSchedulingPolicy struct{
+  "allowStaffSelection": true,
+  "maximumAdvance": "String (timestamp)",
+  "minimumLeadTime": "String (timestamp)",
+  "sendConfirmationsToOwner": true,
+  "timeSlotInterval": "String (timestamp)"
+}
